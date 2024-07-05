8991feb255b84d4aef58b811e1695283039d8ff7
Hussein135-coder <80627987+Hussein135-coder@users.noreply.github.com>
2024-07-05 22:32:12 +0300

    Update index.js

--- index.js
+++ index.js
@@ -113,6 +113,49 @@ app.post('/syria' , async (req,res)=>{
 	}
 })
 
+app.get('/req' , async (req,res)=>{
+  runTest();
+})
+
+const url = "https://2000-01hzkq91ad81aw7qbwnsgv5z49.cloudspaces.litng.ai/";
+const duration = 5000; // Test duration in milliseconds
+const concurrency = 200; // Number of concurrent requests
+
+let requestsSent = 0;
+let responsesReceived = 0;
+
+async function sendRequest() {
+  requestsSent++;
+  try {
+    const response = await fetch(url);
+    const text = await response.text(); // Consume the body
+    console.log(text);
+    responsesReceived++;
+  } catch (error) {
+    console.error("Error:");
+  }
+}
+
+const startTime = Date.now();
+
+async function runTest() {
+  if (Date.now() - startTime < duration) {
+    const requests = Array(concurrency)
+      .fill()
+      .map(() => sendRequest());
+    await Promise.all(requests);
+    setImmediate(runTest);
+  } else {
+    const totalTime = (Date.now() - startTime) / 1000;
+    const rps = responsesReceived / totalTime;
+    console.log(`Requests sent: ${requestsSent}`);
+    console.log(`Responses received: ${responsesReceived}`);
+    console.log(`Test duration: ${totalTime} seconds`);
+    console.log(`Requests per second: ${rps.toFixed(2)}`);
+  }
+}
+
+
 // Number
 const resultNum = async (num,city,category,sub='',halfurl='')=>{
     console.log(num,city,category,sub,halfurl)
